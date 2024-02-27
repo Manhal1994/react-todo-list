64d3bdcc7da72797e0c59a34860ab1692e77a9c7
Manhal khwashki <manhal.akram.khwashki@gmail.com>
2024-02-27 20:29:51 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [x, setx] = useState([]);
+  const inputRef = useRef();
+  const addTodo = () => {
+    const text = inputRef.current.value;
+    const newNote = {
+      text,
+      completed: false,
+    };
+    setx([...x, newNote]);
+  };
+  const toggleTodo = (index) => {
+    const newTodo = [...x];
+    newTodo[index].completed = !newTodo[index].completed;
+    setx(newTodo);
+  };
+  const toDelete = (index) => {
+    const newx = [...x];
+
+    newx.splice(index, 1);
+
+    setx(newx);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <ul>
+        {x.map((todo, index) => {
+          return (
+            <div className="item">
+              {" "}
+              <li
+                className={todo.completed ? "diffstyle" : ""}
+                onClick={() => toggleTodo(index)}
+              >
+                {todo.text}
+              </li>
+              <span onClick={() => toDelete(index)}>X</span>
+            </div>
+          );
+        })}
+      </ul>
+      <input ref={inputRef} type="text" placeholder="Add Todo" />
+      <button onClick={() => addTodo()} value="Add Note">
+        Add Note
+      </button>
     </div>
   );
 }
